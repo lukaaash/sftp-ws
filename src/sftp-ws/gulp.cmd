f9a69571e8ee677456debbbf68d5201a93a903dd
Lukas Pokorny <lukas@nuane.com>
2014-11-10 23:12:36 +0100

    Added build script

---
+++ src/sftp-ws/gulp.cmd
@@ -0,0 +1,1 @@
+@node node_modules\gulp\bin\gulp.js %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ src/sftp-ws/gulpfile.ts
@@ -0,0 +1,36 @@
+﻿import path = require('path');
+var gulp = require('gulp');
+var typescript = require('gulp-tsc');
+
+var paths = {
+    sources: ['**/*.ts', '!**/*.d.ts', '!main.ts', '!node_modules/**', '!gulpfile.*'],
+    scripts: ['client/*.js'],
+    html: ['app/index.html', '!app/test.html'],
+    out: '../../lib'
+};
+
+gulp.task('sources', () => {
+    var options = {
+        declaration: false,
+        removeComments: true
+    };
+    options['module'] = 'commonjs';
+
+    gulp.src(paths.sources)
+        .pipe(typescript(options))
+        .pipe(gulp.dest(paths.out));
+});
+
+gulp.task('typings', () => {
+    //TODO
+});
+
+gulp.task('scripts', () => {
+    gulp.src(paths.scripts)
+        .pipe(gulp.dest(path.join(paths.out, "client")));
+});
+
+gulp.task('default', ['sources', 'scripts'], () => {
+
+});
+
