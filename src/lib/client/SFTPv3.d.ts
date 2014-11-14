5160fa0a07844c44c8e5c063008398e8ba3d37f8
Lukas Pokorny <lukas@nuane.com>
2014-11-14 21:30:25 +0100

    Fixed SFTP client typings

--- src/lib/client/SFTPv3.d.ts
+++ src/lib/client/SFTPv3.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="Stats.d.ts" />
 import events = require("events");
-import Stats = require("Stats");
+import IStats = require("Stats");
 
 declare class SFTP extends events.EventEmitter {
 
@@ -11,21 +11,21 @@ declare class SFTP extends events.EventEmitter {
     end(): void;
 
     // core methods
-    open(path: string, flags: string, attrs?: Stats, callback?: (err: Error, handle: any) => any): void;
+    open(path: string, flags: string, attrs?: IStats, callback?: (err: Error, handle: any) => any): void;
     close(handle: any, callback?: (err: Error) => any): void;
     read(handle: any, buffer: NodeBuffer, offset: number, length: number, position: number, callback?: (err: Error, bytesRead: number, buffer: NodeBuffer) => any): void;
     write(handle: any, buffer: NodeBuffer, offset: number, length: number, position: number, callback?: (err: Error) => any): void;
-    lstat(path: string, callback?: (err: Error, attrs: Stats) => any): void;
-    fstat(handle: any, callback?: (err: Error, attrs: Stats) => any): void;
-    setstat(path: string, attrs: Stats, callback?: (err: Error) => any): void;
-    fsetstat(handle: any, attrs: Stats, callback?: (err: Error) => any): void;
+    lstat(path: string, callback?: (err: Error, attrs: IStats) => any): void;
+    fstat(handle: any, callback?: (err: Error, attrs: IStats) => any): void;
+    setstat(path: string, attrs: IStats, callback?: (err: Error) => any): void;
+    fsetstat(handle: any, attrs: IStats, callback?: (err: Error) => any): void;
     opendir(path: string, callback?: (err: Error, handle: any) => any): void;
-    readdir(handle: any, callback?: (err: Error, items: { filename: string; longname: string; stats?: Stats; }[]) => any): void;
+    readdir(handle: any, callback?: (err: Error, items: { filename: string; longname: string; stats?: IStats; }[]) => any): void;
     unlink(path: string, callback?: (err: Error) => any): void;
-    mkdir(path: string, attrs?: Stats, callback?: (err: Error) => any): void;
+    mkdir(path: string, attrs?: IStats, callback?: (err: Error) => any): void;
     rmdir(path: string, callback?: (err: Error) => any): void;
     realpath(path: string, callback?: (err: Error, resolvedPath: string) => any): void;
-    stat(path: string, callback?: (err: Error, attrs: Stats) => any): void;
+    stat(path: string, callback?: (err: Error, attrs: IStats) => any): void;
     rename(oldPath: string, newPath: string, callback?: (err: Error) => any): void;
     readlink(path: string, callback?: (err: Error, linkString: string) => any): void;
     symlink(targetpath: string, linkpath: string, callback?: (err: Error) => any): void;
--- src/lib/client/Stats.d.ts
+++ src/lib/client/Stats.d.ts
@@ -1,10 +1,11 @@
-﻿declare class Stats {
-    mode: number;
-    uid: number;
-    gid: number;
-    size: number;
-    atime: Date;
-    mtime: Date;
+﻿interface IStats {
+    mode?: number;
+    permissions?: number; // backwards compatiblity
+    uid?: number;
+    gid?: number;
+    size?: number;
+    atime?: Date;
+    mtime?: Date;
 }
 
-export = Stats;
+export = IStats;
