a134b37f2e8a069ac11a5123e9ae7c55f3b1ac47
Lukas Pokorny <lukas@nuane.com>
2015-05-25 23:43:05 +0200

    Added mocha test launching to startup file

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,8 @@
-﻿/// <reference path="typings/ws/ws.d.ts" />
+﻿/// <reference path="typings/node/node.d.ts" />
+/// <reference path="typings/ws/ws.d.ts" />
 /// <reference path="lib/sftp.ts" />
 
-import SFTP = require("./lib/sftp");
+process.argv.push("tests"); require("./node_modules/mocha/bin/_mocha"); // run tests
 
-import SftpServer = SFTP.Server;
-import SftpClient = SFTP.Client;
 
-console.log("Starting SFTP.WS server...");
 
-var port = process.env.port || 1984;
-var host = "0.0.0.0";
-
-var server = new SftpServer({
-    port: port,
-    host: host,
-    //log: console,
-});
-
-console.log("SFTP.WS server running at port %s", port);
-
-var client = new SftpClient("ws://127.0.0.1:" + port);
-
-client.on('error', err => {
-    console.log("client:", "Error : %s", err.message);
-});
-
-client.on('ready', () => {
-
-    console.log("client: Connected.");
-
-    client.readdir(".", (err, list) => {
-        console.log("client:", err);
-        console.log("client:", list);
-        client.end();
-    });
-
-});
