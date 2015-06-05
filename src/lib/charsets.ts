79ddca163aa5858a571761d249262be4b3488245
Lukas Pokorny <lukas@nuane.com>
2015-06-05 23:06:57 +0200

    Added string encoding/decoding objects

---
+++ src/lib/charsets.ts
@@ -0,0 +1,278 @@
+﻿
+export interface IStringEncoder extends StringEncoder {
+}
+
+export interface IStringDecoder extends StringDecoder {
+}
+
+export class Encoding {
+
+    constructor(name: string) {
+        var encoding = (name + "").toLowerCase().replace("-", "");
+        if (encoding != "utf8") throw new Error("Encoding not supported: " + name);
+        //TODO: support ASCII and other encodings in addition to UTF-8
+    }
+
+    static UTF8 = new Encoding("utf8");
+
+    getEncoder(value: string): IStringEncoder {
+        return new StringEncoder(value);
+    }
+
+    getDecoder(): IStringDecoder {
+        return new StringDecoder();
+    }
+
+    encode(value: string, buffer: NodeBuffer, offset: number, end?: number): number {
+        return encodeUTF8(value, buffer, offset, end);
+    }
+
+    decode(buffer: NodeBuffer, offset: number, end?: number): string {
+        return decodeUTF8(buffer, offset, end);
+    }
+}
+
+const enum UnicodeChars {
+    REPLACEMENT_CHAR = 0xFFFD,
+    BOM = 0xFEFF,
+}
+
+class StringEncoder {
+
+    private _value: string;
+    private _code: number;
+    private _length: number;
+    private _position: number;
+    private _done: boolean;
+
+    //TODO: add write():bool, change finish() to end():void, then expect read()
+    finished(): boolean {
+        return this._done;
+    }
+
+    constructor(value: string) {
+        if (typeof value !== "string") value = "" + value;
+        this._value = value;
+    }
+
+    read(buffer: NodeBuffer, offset: number, end?: number): number {
+        return encodeUTF8(this._value, buffer, offset, end, <any>this);
+    }
+}
+
+function encodeUTF8(value: string, buffer: NodeBuffer, offset: number, end?: number, state?: { _code: number; _length: number; _position: number; _done: boolean; }): number {
+    end = end || buffer.length;
+
+    var code: number;
+    var length: number;
+    var position: number;
+    if (state) {
+        code = state._code | 0;
+        length = state._length | 0;
+        position = state._position | 0;
+    } else {
+        code = 0;
+        length = 0;
+        position = 0;
+    }
+
+    var done = false;
+    var start = offset;
+
+    while (true) {
+        if (length > 0) {
+            if (offset >= end) break;
+
+            // emit multi-byte sequences
+            buffer[offset++] = (code >> 12) | 0x80;
+
+            if (length > 1) {
+                code = (code & 0xFFF) << 6;
+                length--;
+                continue;
+            }
+
+            // proceed to next character
+            length = 0;
+            code = 0;
+        }
+
+        // fetch next string if needed
+        if (position >= value.length) {
+            position = 0;
+
+            // if the string ends normally, we are done
+            if (code == 0) {
+                done = true;
+                break;
+            }
+
+            // if the string ends with a lone high surrogate, emit a replacement character instead
+            value = String.fromCharCode(UnicodeChars.REPLACEMENT_CHAR);
+            code = 0;
+        }
+
+        if (offset >= end) break;
+
+        var c = value.charCodeAt(position++);
+        if (code == 0) {
+            code = c;
+
+            // handle high surrogate
+            if (c >= 0xD800 && c < 0xDC00) {
+                code = 0x10000 + ((code & 0x3FF) << 10);
+                continue;
+            }
+
+            // handle lone low surrogate
+            if (c >= 0xDC00 && c < 0xE000) {
+                code = UnicodeChars.REPLACEMENT_CHAR;
+            } else {
+                code = c;
+            }
+        } else {
+            // handle low surrogate
+            if (c >= 0xDC00 && c < 0xE000) {
+                // calculate code
+                code += (c & 0x3FF);
+            } else {
+                // invalid low surrogate
+                code = UnicodeChars.REPLACEMENT_CHAR;
+            }
+        }
+
+        // emit first byte in a sequence and determine what to emit next
+        if (code <= 0x7F) {
+            buffer[offset++] = code;
+            code = 0;
+        } else if (code <= 0x7FF) {
+            length = 1;
+            buffer[offset++] = (code >> 6) | 0xC0;
+            code = (code & 0x3F) << 12;
+        } else if (code <= 0xFFFF) {
+            length = 2;
+            buffer[offset++] = (code >> 12) | 0xE0;
+            code = (code & 0xFFF) << 6;
+        } else if (code <= 0x10FFFF) {
+            length = 3;
+            buffer[offset++] = (code >> 18) | 0xF0;
+            code = (code & 0x1FFFFF);
+        } else {
+            code = UnicodeChars.REPLACEMENT_CHAR;
+            length = 2;
+            buffer[offset++] = (code >> 12) | 0xE0;
+            code = (code & 0xFFF) << 6;
+        }
+    }
+
+    if (state) {
+        state._code = code;
+        state._length = length;
+        state._position = position;
+        state._done = done;
+    } else {
+        if (!done) return -1;
+    }
+
+    return offset - start;
+}
+
+class StringDecoder {
+    private _text: string;
+    private _code: number;
+    private _length: number;
+    private _position: number;
+    private _removeBom: boolean;
+
+    text(): string {
+        return this._text;
+    }
+
+    write(buffer: NodeBuffer, offset: number, end: number): void {
+        var bytes = decodeUTF8(buffer, offset, end, <any>this);
+        var text = this._text;
+
+        if (this._removeBom && text.length > 0) {
+            if (text.charCodeAt(0) == UnicodeChars.BOM) this._text = text.substr(1);
+            this._removeBom = false;
+        }
+    }
+}
+
+function decodeUTF8(buffer: NodeBuffer, offset: number, end?: number, state?: { _text?: string; _code?: number; _length?: number; }): string {
+    end = end || buffer.length;
+
+    var text: string;
+    var code: number;
+    var length: number;
+    if (state) {
+        text = state._text || "";
+        code = state._code | 0;
+        length = state._length | 0;
+    } else {
+        text = "";
+        code = 0;
+        length = 0;
+    }
+
+    while (offset < end) {
+        var b = buffer[offset++];
+
+        if (length > 0) {
+            if ((b & 0xC0) != 0x80) {
+                code = UnicodeChars.REPLACEMENT_CHAR;
+                length = 0;
+            } else {
+                code = (code << 6) | (b & 0x3F);
+                length--;
+                if (length > 0) continue;
+            }
+        } else if (b <= 128) {
+            code = b;
+            length = 0;
+        } else {
+            switch (b & 0xE0) {
+                case 0xE0:
+                    if (b & 0x10) {
+                        code = b & 0x07;
+                        length = 3;
+                    } else {
+                        code = b & 0xF;
+                        length = 2;
+                    }
+                    continue;
+                case 0xC0:
+                    code = b & 0x1F;
+                    length = 1;
+                    continue;
+                default:
+                    code = UnicodeChars.REPLACEMENT_CHAR;
+                    length = 0;
+                    break;
+            }
+        }
+
+        // emit surrogate pairs for supplementary plane characters
+        if (code >= 0x10000) {
+            code -= 0x10000;
+            if (code > 0xFFFFF) {
+                code = UnicodeChars.REPLACEMENT_CHAR;
+            } else {
+                text += String.fromCharCode(0xD800 + ((code >> 10) & 0x3FF));
+                code = 0xDC00 + (code & 0x3FF);
+            }
+        }
+
+        text += String.fromCharCode(code);
+    }
+
+    if (state) {
+        state._code = code;
+        state._length = length;
+        state._text = text;
+        return null;
+    } else {
+        if (length > 0) text += String.fromCharCode(UnicodeChars.REPLACEMENT_CHAR);
+        return text;
+    }
+}
---
+++ src/tests/charset-tests.ts
@@ -0,0 +1,135 @@
+﻿/// <reference path="../typings/mocha/mocha.d.ts" />
+import assert = require('assert');
+import util = require("../lib/charsets");
+
+import Encoding = util.Encoding;
+
+var UTF8 = Encoding.UTF8;
+
+describe("Encoding Tests", function () {
+
+    before(done => {
+        done();
+    });
+
+    after(done => {
+        done();
+    });
+
+    // incomplete chunks
+    var chunk1 = new Buffer([0xc5, 0xbd, 0x6c, 0x75, 0xc5, 0xa5, 0x6f, 0x75, 0xc4, 0x8d, 0x6b, 0xc3, 0xbd, 0x20, 0x6b, 0xc5]); // "Žluťoučký k" + incomplete 'ů'
+    var text1 = "Žluťoučký k";
+    var chunk2 = new Buffer([0xaf, 0xc5, 0x88, 0x20, 0xc3, 0xba, 0x70, 0xc4, 0x9b, 0x6c, 0x20, 0xc4]); // incomplete 'ů' + "ň úpěl " + incomplete 'ď'
+    var text2 = "ň úpěl ";
+    var chunk3 = new Buffer([0x8f, 0xc3, 0xa1, 0x62, 0x65, 0x6c, 0x73, 0x6b, 0xc3, 0xa9, 0x20, 0xc3, 0xb3, 0x64, 0x79]); // incomplete 'ď' + "ábelské ódy"
+    var text3 = "ábelské ódy";
+
+    // mix of two-byte and one-byte characters
+    var chunk4 = Buffer.concat([chunk1, chunk2, chunk3]);
+    var text4 = "Žluťoučký kůň úpěl ďábelské ódy";
+
+    // three-byte characters
+    var chunk5 = new Buffer([0xE6, 0xAD, 0xBB, 0xE9, 0xA9, 0xAC, 0xE5, 0xBD, 0x93, 0xE6, 0xB4, 0xBB, 0xE9, 0xA9, 0xAC, 0xE5, 0x8C, 0xBB]);
+    var text5 = "死马当活马医";
+
+    // surrogate pairs
+    var chunk6 = new Buffer([0xF0, 0xA0, 0x9C, 0x8E, 0xF0, 0xA0, 0x9D, 0xB9, 0xF0, 0xA0, 0xB3, 0x8F, 0xF0, 0xA1, 0x81, 0xBB, 0xF0, 0xA9, 0xB6, 0x98]);
+    var text6 = "𠜎𠝹𠳏𡁻𩶘";
+
+    var BAD_CHAR = String.fromCharCode(0xFFFD); // REPLACEMENT_CHAR
+
+    function assertEqualContents(actual: NodeBuffer, expected: NodeBuffer): void {
+        var len = Math.min(actual.length, expected.length);
+        var same = true;
+
+        if (actual.length != expected.length) {
+            same = false;
+        } else {
+            for (var i = 0; i < len; i++) {
+                if (actual[i] != expected[i]) {
+                    same = false;
+                    break;
+                }
+            }
+        }
+
+        if (!same) assert.equal(actual, expected);
+    }
+
+    it("encode2byte",() => {
+        var buffer = new Buffer(1024);
+        var count = UTF8.encode(text4, buffer, 0);
+        var actual = buffer.slice(0, count);
+        assertEqualContents(actual, chunk4);
+    });
+
+    it("encode3byte",() => {
+        var buffer = new Buffer(1024);
+        var count = UTF8.encode(text5, buffer, 0);
+        var actual = buffer.slice(0, count);
+        assertEqualContents(actual, chunk5);
+    });
+
+    it("encode4byte",() => {
+        var buffer = new Buffer(1024);
+        var count = UTF8.encode(text6, buffer, 0);
+        var actual = buffer.slice(0, count);
+        assertEqualContents(actual, chunk6);
+    });
+
+    it("encodeTooLong",() => {
+        var buffer = new Buffer(1024);
+        var count = UTF8.encode(text6, buffer, 0, 3);
+        assert.equal(count, -1);
+    });
+
+    it("encodeChunked",() => {
+        var buffer = new Buffer(1024);
+        var encoder = UTF8.getEncoder(text6);
+        var offset = 0;
+        [3, 6, 7, 4].forEach(bytes => {
+            var count = encoder.read(buffer, offset, offset + bytes);
+            assert.equal(count, bytes);
+            assertEqualContents(buffer.slice(offset, offset + count), chunk6.slice(offset, offset + count));
+            offset += count;
+        });
+    });
+
+    it("decode2byte",() => {
+        var actual = UTF8.decode(chunk4, 0, chunk4.length);
+        assert.equal(actual, text4);
+    });
+
+    it("decode3byte",() => {
+        var actual = UTF8.decode(chunk5, 0, chunk5.length);
+        assert.equal(actual, text5);
+    });
+
+    it("decode4byte",() => {
+        var actual = UTF8.decode(chunk6, 0, chunk6.length);
+        assert.equal(actual, text6);
+    });
+
+    it("decodeIncompleteEnd",() => {
+        var actual = UTF8.decode(chunk1, 0, chunk1.length);
+        assert.equal(actual, text1 + BAD_CHAR);
+    });
+
+    it("decodeIncompleteBoth",() => {
+        var actual = UTF8.decode(chunk2, 0, chunk2.length);
+        assert.equal(actual, BAD_CHAR + text2 + BAD_CHAR);
+    });
+
+    it("decodeWithState",() => {
+        var decoder = UTF8.getDecoder();
+
+        decoder.write(chunk1, 0, chunk1.length);
+        decoder.write(chunk2, 0, chunk2.length);
+        decoder.write(chunk3, 0, chunk3.length);
+
+        var actual = decoder.text();
+        assert.equal(actual, text4);
+    });
+
+
+});
