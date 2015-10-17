fb924af2b5f782a41019b671432e1a4cc394e68d
Lukas Pokorny <lukas.pokorny@rebex.net>
2015-10-17 12:10:35 +0200

    Optimized browser-based event emitter

--- src/lib/util-web.ts
+++ src/lib/util-web.ts
@@ -24,12 +24,19 @@ class EventEmitter {
         if (!emitter || typeof emitter._events === "undefined") return 0;
         var list = <Function[]>emitter._events[event];
         if (!list) return 0;
+        if (typeof list === "function") return 1;
         return list.length;
     }
 
     addListener(event: string, listener: Function): EventEmitter {
-        var list = <Function[]>this._events[event] || [];
-        list.push(listener);
+        var list = <Function[]>this._events[event];
+        if (!list) {
+            list = <any>listener;
+        } else if (typeof list === "function") {
+            list = [<any>list, listener];
+        } else {
+            list.push(listener);
+        }
         this._events[event] = list;
         return this;
     }
@@ -49,12 +56,15 @@ class EventEmitter {
 
     removeListener(event: string, listener: Function): EventEmitter {
         var list = <Function[]>this._events[event];
-        if (!Array.isArray(list))
-            return;
-
-        var n = list.indexOf(listener);
-        if (n >= 0)
-            list.splice(n, 1);
+        if (typeof list === "function") {
+            if (<any>list === listener) {
+                delete this._events[event];
+            } else if (Array.isArray(list)) {
+                var n = list.indexOf(listener);
+                if (n >= 0) list.splice(n, 1);
+                if (list.length == 0) delete this._events[event];
+            }
+        }
 
         return this;
     }
@@ -69,22 +79,73 @@ class EventEmitter {
     }
 
     listeners(event: string): Function[] {
-        return this._events[event];
+        var list = this._events[event];
+        if (typeof list === "function") {
+            list = [<any>list];
+        } else if (!Array.isArray(list)) {
+            list = [];
+        }
+
+        return list;
     }
 
-    emit(event: string, ...args: any[]): boolean {
+    emit(event: string, ...args: any[]): boolean
+    emit(event: string): boolean {
         var list = <Function[]>this._events[event];
+        if (typeof list === "function") {
+            var listener = <Function><any>list;
+            switch (arguments.length) {
+                case 1:
+                    listener.call(null);
+                    break;
+                case 2:
+                    listener.call(null, arguments[1]);
+                    break;
+                case 3:
+                    listener.call(null, arguments[1], arguments[2]);
+                    break;
+                case 4:
+                    listener.call(null, arguments[1], arguments[2], arguments[3]);
+                    break;
+                default:
+                    var args = Array.prototype.slice(1);
+                    listener.apply(null, args);
+                    break;
+            }
+            return true;
+        }
+
         var called = false;
         if (Array.isArray(list)) {
             for (var i = 0; i < list.length; i++) {
-                list[i].apply(null, args);
+                var listener = <Function>list[i];
+                switch (arguments.length) {
+                    case 1:
+                        listener.call(null);
+                        break;
+                    case 2:
+                        listener.call(null, arguments[1]);
+                        break;
+                    case 3:
+                        listener.call(null, arguments[1], arguments[2]);
+                        break;
+                    case 4:
+                        listener.call(null, arguments[1], arguments[2], arguments[3]);
+                        break;
+                    default:
+                        var args = Array.prototype.slice(1);
+                        listener.apply(null, args);
+                        break;
+                }
                 called = true;
             }
         }
+
         if (!called && event == "error") {
             var error = <Error>args[0];
             throw error;
         }
+
         return called;
     }
 }
